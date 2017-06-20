a543ead75442ca9135dcf963c0a0dba14c9200ac
jojo <jojo@chew-i7.fuhsd.org>
2017-06-19 20:34:56 -0700

    PreRoundDiagnostics changes
    Removed PreRoundDiagnostics Command and changed it to
    PreRoundDiagnostics CommandGroup. It is currently Blank.

--- src/org/usfirst/frc/team2473/robot/commands/PreRoundDiagnostics.java
+++ src/org/usfirst/frc/team2473/robot/commands/PreRoundDiagnostics.java
@@ -1,40 +1,8 @@
 package org.usfirst.frc.team2473.robot.commands;
 
-import org.usfirst.frc.team2473.robot.Robot;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-import edu.wpi.first.wpilibj.command.Command;
-
-/**
- *
- */
-public class PreRoundDiagnostics extends Command {
-
-    public PreRoundDiagnostics() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.driveTrain);
-    }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	System.out.println("Diagnostics interrupted?");
-    }
+public class PreRoundDiagnostics extends CommandGroup{
+	public PreRoundDiagnostics(){
+	}
 }
--- src/org/usfirst/frc/team2473/robot/commands/TestGroup.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team2473.robot.commands;
-
-import edu.wpi.first.wpilibj.command.CommandGroup;
-
-public class TestGroup extends CommandGroup{
-	public TestGroup(){
-	}
-}
