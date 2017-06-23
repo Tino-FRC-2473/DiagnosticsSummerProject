685a4bdb75b61367f24515b383d5b7f0691204a1
Jonathan Ho <jonathan.ho.ca@gmail.com>
2017-06-22 20:24:17 -0700

    Added more subsystems and finished preround diagnostics file reading!

--- src/org/usfirst/frc/team2473/robot/commands/PreRoundDiagnostics.java
+++ src/org/usfirst/frc/team2473/robot/commands/PreRoundDiagnostics.java
@@ -1,8 +1,40 @@
 package org.usfirst.frc.team2473.robot.commands;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class PreRoundDiagnostics extends CommandGroup{
-	public PreRoundDiagnostics(){
+	public PreRoundDiagnostics() throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader("Robot.txt"));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String in = st.nextToken();
+			switch (in) {
+				case "talon" :
+					addSequential(new DriveTrainTest());
+					break;
+				case "servo" :
+					addSequential(new ServoTest());
+					break;
+				case "gyro" :
+					addSequential(new GyroTest());
+					break;
+				case "light" :
+					addSequential(new LightTest());
+					break;
+				case "breakbeam" :
+					addSequential(new BreakBeamTest());
+					break;
+				default:
+					System.out.println("The following part was not found: " + in + "!");
+			}
+		}
 	}
 }
--- src/org/usfirst/frc/team2473/robot/commands/ServoTest.java
+++ src/org/usfirst/frc/team2473/robot/commands/ServoTest.java
@@ -1,45 +1,36 @@
 package org.usfirst.frc.team2473.robot.commands;
 
-import java.util.Scanner;
-
-import org.usfirst.frc.team2473.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class GyroTest extends Command {
-	private Scanner scan;
-	
-    public GyroTest() {
+public class ServoTest extends Command {
+
+    public ServoTest() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(Robot.gyroSystem);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	System.out.println("Testing gyro sensor");
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-		System.out.println("Current gyro value: " + Robot.gyroSystem.getValue());
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-		return scan.nextLine().equals("\n");    }
+        return false;
+    }
 
     // Called once after isFinished returns true
     protected void end() {
-    	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
     }
 }
